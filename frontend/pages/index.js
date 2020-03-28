3ad825efe70b4b98b3acb3be6b3c8a71037a66b9
Marcin Dabrowski <marcin@Marcins-MacBook-Pro.local>
2020-03-28 12:59:01 -0500

    install prettier and eslint

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,60 +1,12 @@
 import Router from 'next/router';
 import React, {useRef, useState} from 'react';
 
-const endpoint = 'http://localhost:1337/auth/local/register';
-
 export default () => {
-  const loginRef = useRef();
-  const emailRef = useRef();
-  const pwdRef = useRef();
-  const [error, setError] = useState('');
-
-  // Using fetch to pass the values to the Strapi API
-  const register = async () => {
-    const username = loginRef.current.value;
-    const email = emailRef.current.value;
-    const password = pwdRef.current.value;
-
-    console.log({username, email, password});
-
-    // error handling
-    try {
-      const {jwt, user} = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username,
-          email,
-          password,
-        }),
-      }).then(res => {
-        if (res.status !== 200) {
-          throw new Error('Error logging in, non 200 code');
-        }
-        return res.json();
-      });
-
-      window.sessionStorage.setItem('jwt', jwt);
-      window.sessionStorage.setItem('user', JSON.stringify(user));
-
-      Router.push('/');
-    } catch (e) {
-      setError(e.toString());
+  //what is useEffect in react?
+  useEffect(() => {
+    const jwt = window.sessionStorage.getItem('jwt');
+    if (!jwt) {
+      Router.push('/login');
     }
-  };
-
-  return (
-    <div>
-      <h1>welcome home</h1>
-      <div style={{display: 'flex', flexDirection: 'column', width: 300}}>
-        <input type="text" placeholder="Login" ref={loginRef} />
-        <input type="text" placeholder="Email" ref={emailRef} />
-        <input type="password" placeholder="Password" ref={pwdRef} />
-        <button onClick={() => register()}>Register</button>
-      </div>
-      {error && <div style={{border: '1px red solid'}}>{error}</div>}
-    </div>
-  );
-};
+  }, []);
+}
