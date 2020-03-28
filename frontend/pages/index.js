ef870d2b4ffcaba9a5d57b0468dd619ea7b1213d
Marcin Dabrowski <marcin@Marcins-MacBook-Pro.local>
2020-03-28 14:53:17 -0500

    create todo

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,19 +1,55 @@
 import Router from 'next/router';
 import React, {useEffect, useRef, useState} from 'react';
 
+const endpoint = 'http://localhost:1337/todos'
+
 export default () => {
+  const titleRef = useRef;
+  const jwtRef = useRef;
+  const [todos, setTodos] = useState([]);
+
   //what is useEffect in react?
   useEffect(() => {
     const jwt = window.sessionStorage.getItem('jwt');
     if (!jwt) {
       Router.push('/login');
     }
+    jwtRef.current = jwt;
   }, []);
-  return (
-    <div style={{display: "flex", flexDirection: "column", width: 200}}>
-      <input type="text" placeholder="tile" />
 
-      <button>Create todo</button>
+
+  const createTodo = async () => {
+    const title = titleRef.current.value;
+
+    const res = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${jwtRef.current}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+      }),
+    }).then(r => r.json());
+
+    const newTodos = [...todos, res];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <div style={{display: "flex", flexDirection: "column", width: 200}}>
+        <input type="text" placeholder="tile" />
+
+        <button onClick={() => createTodo()}>Create todo</button>
+      </div>
+
+      <div>
+        <h2> todos </h2>
+        <pre> {JSON.stringify(todos)} </pre>
+
+      </div>
     </div>
   )
+  // what is stringify? 
 }
